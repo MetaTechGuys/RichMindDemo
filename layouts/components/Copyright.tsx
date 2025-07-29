67a027f86a8ec6ebdf02bc39bfdbfa01e047e4de
Ali Ghorbani <alidoubleplus@gmail.com>
2025-07-29 16:55:34 +0330

    add version too footer copybar

--- layouts/components/Copyright.tsx
+++ layouts/components/Copyright.tsx
@@ -1,7 +1,16 @@
+import pckg from '@/package.json';
+
 export default function Copyright() {
   return (
     <div className="font-display bg-neutral-800 py-2 text-center text-xs text-white sm:text-sm md:text-lg">
-      ©2024 Rich Mind Holding | All Rights Reserved
+      <span className="inline-flex flex-wrap gap-1">
+        <span>©2024</span>
+        <span>Rich Mind Holding</span>
+        <span>|</span>
+        <span title={new Date(pckg['version.stamp']).toLocaleString()}>v{pckg.version}</span>
+        <span>|</span>
+        <span>All Rights Reserved</span>
+      </span>
     </div>
   );
 }
